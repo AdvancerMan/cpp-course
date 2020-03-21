b0125ae53946b916c07af5c1ccb453814cacbb53
AdvancerMan <advancerman@gmail.com>
2020-03-21 17:37:45 +0300

    .

--- practice2/build.sh
+++ practice2/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-echo g++ -std=c++17 -o out main.cpp
-g++ -std=c++17 -o out main.cpp
+DIR=$(dirname "$0")
+echo g++ -std=c++17 -o $DIR/out $DIR/main.cpp
+g++ -std=c++17 -o $DIR/out $DIR/main.cpp
+
 
